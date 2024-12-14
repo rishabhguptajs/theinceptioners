37caaa9b5ffee3197d70e2f137ee53dd6a80dc40
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-12-14 18:38:17 +0530

    initialised backend files

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected: ${conn.connection.name}`);
   } catch (error) {
     console.log(`MongoDB connection error: ${error.message}`);
   }
